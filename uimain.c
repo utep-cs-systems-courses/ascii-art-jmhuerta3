572b991c2c4398b7bf53d45fa226fb1c4926224c
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@Jazmins-MacBook-Air-3.local>
2024-02-06 17:35:40 -0700

    arrow first attempt

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -29,6 +29,11 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'a':
+      puts("You selected arrow:");
+      print_triangle(5,7);
+      print_square(5,5);
+      break;
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
@@ -42,3 +47,4 @@ int main()
  done: // To exit from program
   return 0;
 }
+
